764f661a5c55a187946000588260392704af1f06
ryan <ryanlab03@gmail.com>
2021-07-16 09:24:51 +0200

    All circuits list

--- src/Controller/CircuitController.php
+++ src/Controller/CircuitController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Circuit;
-use App\Form\CircuitType;
 use App\Repository\CircuitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,14 +20,40 @@ class CircuitController extends AbstractController
      */
     public function index(CircuitRepository $circuitRepository): Response
     {
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', 'http://ergast.com/api/f1/circuits.json');
+
+        if (200 == $response->getStatusCode()) {
+            $data = json_decode($response->getContent(), true);
+            $circuits = $data['MRData']['CircuitTable']['Circuits'];
+
+           foreach ($circuits as $circuit) {
+               $this->newFromAPI($circuit);
+           }
+        }
+
         return $this->render('circuit/index.html.twig', [
             'circuits' => $circuitRepository->findAll(),
         ]);
     }
 
+    public function newFromAPI(array $object)
+    {
+        $circuit = new Circuit();
+
+        $circuit->setName($object['circuitName']);
+        $circuit->setCountry($object['Location']['country']);
+        $circuit->setLocality($object['Location']['locality']);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($circuit);
+        $entityManager->flush();
+    }
+
+    /*
     /**
      * @Route("/new", name="circuit_new", methods={"GET","POST"})
-     */
+     *./
     public function new(Request $request): Response
     {
         $circuit = new Circuit();
@@ -50,7 +76,7 @@ class CircuitController extends AbstractController
 
     /**
      * @Route("/{id}", name="circuit_show", methods={"GET"})
-     */
+     *./
     public function show(Circuit $circuit): Response
     {
         return $this->render('circuit/show.html.twig', [
@@ -60,7 +86,7 @@ class CircuitController extends AbstractController
 
     /**
      * @Route("/{id}/edit", name="circuit_edit", methods={"GET","POST"})
-     */
+     *./
     public function edit(Request $request, Circuit $circuit): Response
     {
         $form = $this->createForm(CircuitType::class, $circuit);
@@ -80,7 +106,7 @@ class CircuitController extends AbstractController
 
     /**
      * @Route("/{id}", name="circuit_delete", methods={"POST"})
-     */
+     *./
     public function delete(Request $request, Circuit $circuit): Response
     {
         if ($this->isCsrfTokenValid('delete'.$circuit->getId(), $request->request->get('_token'))) {
@@ -91,4 +117,5 @@ class CircuitController extends AbstractController
 
         return $this->redirectToRoute('circuit_index', [], Response::HTTP_SEE_OTHER);
     }
+    */
 }
